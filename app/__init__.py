d97d01ead8d0e519f25cc8ba67b3831f585336a7
hailtonDavid <hailton.david@gmail.com>
2024-01-08 16:47:33 -0300

    Atualização

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from app.views import homepage
+from app.views import index
--- app/views.py
+++ app/views.py
@@ -1,7 +1,15 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for
 
 
 @app.route("/")
-def homepage():
-    return render_template("index.html")
+def index():
+    nome = "João"
+    idade = 30
+    interesses = ["Python", "Flask", "Desenvolvimento Web"]
+    return render_template("index.html", nome=nome, idade=idade, interesses=interesses)
+
+
+@app.route("/nova")
+def novapagina():
+    return "Sobre nós"
